df94feb1bae865029b8e659290791b008e2ace15
haratemakeria <contato@haratemakeria.com.br>
2025-06-12 17:15:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Painel de Pedidos</title>
+  <title>Painel de Pedidos - Hara Temakeria</title>
   <link rel="stylesheet" href="style.css" />
 </head>
 <body>
-  <h1>Painel de Pedidos - Hara Temakeria</h1>
-  <div class="painel">
+  <header>
+    <h1>Painel de Pedidos - Hara Temakeria</h1>
+  </header>
+
+  <main>
     <div class="coluna" id="analise">
       <h2>Análise</h2>
     </div>
+
     <div class="coluna" id="producao">
       <h2>Produção</h2>
     </div>
+
     <div class="coluna" id="entrega">
       <h2>Pronto para Entrega</h2>
     </div>
-  </div>
+  </main>
+
   <script src="painel.js"></script>
 </body>
 </html>
